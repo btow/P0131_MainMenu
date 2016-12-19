d2b4199fbad4dfa1abb9aa6b7b263438364a4493
btow <btow@yandex.ru>
2016-12-19 19:09:21 +0500

    Initial commit

---
+++ app/src/main/java/com/example/samsung/p0131_mainmenu/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.samsung.p0131_mainmenu;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvOut;
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        tvOut = (TextView) findViewById(R.id.tvOut);
+        imageView = (ImageView) findViewById(R.id.imageView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch (id) {
+            case R.id.action_settings :
+                tvOut.setText(getString(R.string.hello_world));
+                imageView.setImageResource(R.mipmap.ic_launcher);
+                Toast.makeText(MainActivity.this, getString(R.string.action_settings), Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_item1 :
+                tvOut.setText(getString(R.string.action_item1));
+                imageView.setImageResource(R.drawable.img_20130703_185525);
+                Toast.makeText(MainActivity.this, getString(R.string.action_item1), Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_item2 :
+                tvOut.setText(getString(R.string.action_item2));
+                imageView.setImageResource(R.drawable.img_20150305_194201);
+                Toast.makeText(MainActivity.this, getString(R.string.action_item2), Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_item3 :
+                tvOut.setText(getString(R.string.action_item3));
+                imageView.setImageResource(R.mipmap.ic_launcher);
+                Toast.makeText(MainActivity.this, getString(R.string.action_item3), Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
